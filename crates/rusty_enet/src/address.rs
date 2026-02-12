f159d11db82ff9db0d438582c54872aca29f64d2
Barış Şenel <senelb705@gmail.com>
2026-02-12 22:09:59 +0300

    rusty_enet crate

---
+++ crates/rusty_enet/src/address.rs
@@ -0,0 +1,108 @@
+use core::net::{SocketAddr, SocketAddrV4, SocketAddrV6};
+use std::net::{IpAddr, Ipv4Addr};
+
+/// An address type, for use with the [`Socket`](`crate::Socket`) trait.
+pub trait Address: Sized + Clone {
+    /// Are the two addresses the same host?
+    ///
+    /// For IP based addresses, this checks if the IP of two addresses are the same.
+    fn same_host(&self, other: &Self) -> bool;
+    /// Are the two addresses exactly the same?
+    ///
+    /// For IP based addresses, this checks if the IP and port of two addresses are the same.
+    fn same(&self, other: &Self) -> bool;
+    /// Is this a broadcast address?
+    ///
+    /// For IP based addresses, checks if this is the IPv4 broadcast address.
+    fn is_broadcast(&self) -> bool;
+    /// Returns the port number associated with this address.
+    fn port(&self) -> u16;
+    /// Returns the IP address associated with this address.
+    fn address(&self) -> IpAddr;
+}
+
+impl Address for () {
+    fn same_host(&self, _other: &()) -> bool {
+        true
+    }
+
+    fn same(&self, _other: &()) -> bool {
+        true
+    }
+
+    fn is_broadcast(&self) -> bool {
+        false
+    }
+    fn port(&self) -> u16 {
+        0
+    }
+    fn address(&self) -> IpAddr {
+        IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0))
+    }
+}
+
+impl Address for SocketAddrV4 {
+    fn same_host(&self, other: &Self) -> bool {
+        self.ip() == other.ip()
+    }
+
+    fn same(&self, other: &Self) -> bool {
+        *self == *other
+    }
+
+    fn is_broadcast(&self) -> bool {
+        self.ip().is_broadcast()
+    }
+
+    fn port(&self) -> u16 {
+        0
+    }
+    fn address(&self) -> IpAddr {
+        IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0))
+    }
+}
+
+impl Address for SocketAddrV6 {
+    fn same_host(&self, other: &Self) -> bool {
+        self.ip() == other.ip()
+    }
+
+    fn same(&self, other: &Self) -> bool {
+        *self == *other
+    }
+
+    fn is_broadcast(&self) -> bool {
+        false
+    }
+
+    fn port(&self) -> u16 {
+        0
+    }
+    fn address(&self) -> IpAddr {
+        IpAddr::V6(self.ip().clone())
+    }
+}
+
+impl Address for SocketAddr {
+    fn same_host(&self, other: &SocketAddr) -> bool {
+        self.ip() == other.ip()
+    }
+
+    fn same(&self, other: &SocketAddr) -> bool {
+        *self == *other
+    }
+
+    fn is_broadcast(&self) -> bool {
+        match self {
+            SocketAddr::V4(self_addr_v4) => self_addr_v4.ip().is_broadcast(),
+            _ => false,
+        }
+    }
+
+    fn port(&self) -> u16 {
+        self.port()
+    }
+    fn address(&self) -> IpAddr {
+        self.ip()
+    }
+}
